1dcf76c31b524b41c6f1060a58c1affc8ecd895a
thomas vibourel <thomas.vibourel@gmail.com>
2025-05-21 10:12:29 +0200

    tabtop

--- App.js
+++ App.js
@@ -7,6 +7,14 @@ import {
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import AccountScreen from "./screens/AccountScreen";
 import MapScreen from "./screens/MapScreen";
+import SubScreen from "./screens/SubScreen";
+import SigninScreen from "./screens/SignScreen";
+import PriceScreen from "./screens/PriceScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import ListScreen from "./screens/ListScreen";
+import CartScreen from "./screens/CartScreen";
+import ArtScreen from "./screens/ArtScreen";
+import ConnectionScreen from "./screens/ConnectionScreen";
 
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -20,6 +28,28 @@ const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 const TabTop = createTopTabNavigator();
 
+const TabTopNavigator = () => {
+  return (
+    <TabTop.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
+
+          if (route.name === "Connection") {
+            iconName = "sign-in";
+          }
+
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#ec6e5b",
+        tabBarInactiveTintColor: "#335561",
+        headerShown: false,
+      })}
+    >
+      <TabTop.Screen name="Connection" component={ConnectionScreen} />
+    </TabTop.Navigator>
+  );
+};
 const TabNavigator = () => {
   return (
     <Tab.Navigator
@@ -51,6 +81,7 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="TabTopNavigator" component={TabTopNavigator} />
           <Stack.Screen name="Map" component={MapScreen} />
           <Stack.Screen name="TabNavigator" component={TabNavigator} />
         </Stack.Navigator>
